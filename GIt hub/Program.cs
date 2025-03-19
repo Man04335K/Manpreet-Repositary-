93679fc2b873f46b1c17d5317d190690076ac481
Manpreet Kaur <850002581@nzse.ac.nz>
2025-03-19 18:54:45 +1300

    testing git

--- GIt hub/Program.cs
+++ GIt hub/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static long Factorial(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * Factorial(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number;
+        if (int.TryParse(Console.ReadLine(), out number) && number >= 0)
+        {
+            Console.WriteLine($"Factorial of {number} is {Factorial(number)}");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid non-negative integer.");
+        }
+    }
+}
